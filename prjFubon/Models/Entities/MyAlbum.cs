cab7f3d0e5bb5462de43760e71e1baa2827d7086
老朱 <vincent41103@gmail.com>
2024-01-06 12:47:46 +0800

    加入專案檔案。

---
+++ prjFubon/Models/Entities/MyAlbum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prjFubon.Models.Entities;
+
+public partial class MyAlbum
+{
+    public int Id { get; set; }
+
+    public string PicName { get; set; } = null!;
+
+    public byte[] Image { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+}
---
+++ prjFubon/Models/Entities/MyAlbumCategory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prjFubon.Models.Entities;
+
+public partial class MyAlbumCategory
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+}
