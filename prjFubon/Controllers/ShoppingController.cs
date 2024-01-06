8720330e8282acfcaa5e0ff610fa85c7a3bf7e3b
老朱 <vincent41103@gmail.com>
2024-01-06 12:56:20 +0800

    AddController

--- prjFubon/Controllers/ShoppingController.cs
+++ prjFubon/Controllers/ShoppingController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjFubon.Models.Entities;
 
 namespace prjFubon.Controllers
 {
     public class ShoppingController : Controller
     {
+        private readonly IWebHostEnvironment _host;
+        private readonly NorthwindContext _db;
+        public ShoppingController(IWebHostEnvironment host, NorthwindContext context)
+        {
+            _host = host;
+            _db = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult List()
+        {
+            
         }
     }
 }
